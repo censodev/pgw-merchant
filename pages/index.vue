f5a42e261e7a1c69d7048fa297b1fdd8d9b908e7
Phuong Bui <buiphuongaptutc@gmail.com>
2023-08-29 17:14:05 +0700

    index

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,27 @@
 <template>
-
+    <div class="p-10 flex flex-col items-center gap-10">
+        <NuxtLink v-for="gw in gws" :to="gw.link">
+            <img :style="{ height: gw.h }" :src="gw.logo" alt="" srcset="">
+        </NuxtLink>
+    </div>
 </template>
 
 <script setup lang="ts">
-
+const gws = [
+    {
+        link: '/payos',
+        logo: '/img/logopayos.svg',
+        h: '5rem',
+    },
+    {
+        link: '/zalopay',
+        logo: '/img/logozalopay.png',
+        h: '3.75rem'
+    },
+    {
+        link: '/vnpay',
+        logo: '/img/logovnpay.png',
+        h: '4.5rem'
+    },
+]
 </script>
