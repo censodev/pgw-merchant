03615243ac93f8a2e0ce330b17e476f190f8209e
Phuong Bui <buiphuongaptutc@gmail.com>
2023-08-29 16:51:00 +0700

    payos

--- composables/usePayOS.ts
+++ composables/usePayOS.ts
@@ -56,6 +56,28 @@ export type WebhookReq = {
     virtualAccountNumber: string
 }
 
+export type PaymentInfo = {
+    orderCode: number
+    amount: number
+    amountPaid: number
+    amountRemaining: number
+    status: string
+    createdAt: string
+    transactions: {
+        amount: number
+        description: string
+        accountNumber: string
+        reference: string
+        transactionDateTime: string
+        counterAccountBankId: string
+        counterAccountBankName: string
+        counterAccountName: string
+        counterAccountNumber: string
+        virtualAccountName: string
+        virtualAccountNumber: string
+    }[]
+}
+
 export type CallbackParams = {
     code: '00' | string
     id: string
@@ -74,7 +96,7 @@ export default () => {
         const stringifyData = Object.keys(data).filter(key => key !== 'signature').sort().map(key => `${key}=${data[key]}`).join('&')
         return CryptoJS.HmacSHA256(stringifyData, checksumKey).toString(CryptoJS.enc.Hex)
     }
-    function verify(data: any, incomingSignature: string) {
+    function verifySignature(data: any, incomingSignature: string) {
         return sign(data) === incomingSignature
     }
     function createPayment(data: PaymentCreateReq) {
@@ -83,18 +105,30 @@ export default () => {
             method: 'POST',
             headers: { ...authHeaders },
             body: { ...data },
-            onRequest: ({ request, options }) => {
-                console.log('REQ HEADERS >>', options.headers);
-                console.log('REQ BODY >>', options.body);
+            onRequest: ({ options }) => {
+                console.log('REQ HEADERS >>', options.headers)
+                console.log('REQ BODY >>', options.body)
             },
-            onResponse: ({ request, options, response }) => {
-                console.log('RES STATUS >>', response.status);
-                console.log('RES HEADERS >>', response.headers);
-                console.log('RES BODY >>', response._data);
+            onResponse: ({ response }) => {
+                console.log('RES STATUS >>', response.status)
+                console.log('RES HEADERS >>', response.headers)
+                console.log('RES BODY >>', response._data)
+            },
+        })
+    }
+    function getPayment(orderCode: number) {
+        return useFetch<PayOSRes<PaymentInfo>>(`${baseUrl}/v2/payment-requests/${orderCode}`, {
+            headers: { ...authHeaders },
+            onResponse: ({ response }) => {
+                console.log('RES STATUS >>', response.status)
+                console.log('RES HEADERS >>', response.headers)
+                console.log('RES BODY >>', response._data)
             },
         })
     }
     return {
+        verifySignature,
         createPayment,
+        getPayment,
     }
 }
--- pages/payos/callback.vue
+++ pages/payos/callback.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="p-5">
+    <div class="p-5 flex flex-col gap-3">
         <UFormGroup size="xl" label="Callback">
             <UTextarea :rows="7" :value="prettyData" disabled />
+        </UFormGroup>
+        <UFormGroup size="xl" label="Payment Info">
+            <UTextarea :rows="30" :value="prettyPayData" disabled />
         </UFormGroup>
     </div>
 </template>
@@ -12,4 +15,6 @@ import { CallbackParams } from '~/composables/usePayOS';
 const route = useRoute()
 const data = route.query as unknown as CallbackParams
 const prettyData = JSON.stringify(data, undefined, 4)
+const { data: payData } = await usePayOS().getPayment(+data.orderCode)
+const prettyPayData = computed(() => JSON.stringify(payData.value, undefined, 4))
 </script>
